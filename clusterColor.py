3a652f63a0b4c33f4511817e6b0640f9cd5ac3b9
rmqlife <rmqlife@gmail.com>
2016-06-18 21:39:07 +0800

    cluster and show

--- clusterColor.py
+++ clusterColor.py
@@ -23,22 +23,36 @@ def plot_colors(hist, centroids):
 		startX = endX
 	return bar
 
-im = cv2.imread('data/1.jpg')
-for i in xrange(4):
-	im = cv2.pyrDown(im)
-cv2.imshow('im', im)
+def is_black(s): 
+	return np.array_equal(s, np.array([0,0,0]))			
 
 
-# im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-# reshape im to a vector of pixels
-im = im.reshape((im.shape[0] * im.shape[1], 3))
-# cluster the pixel intensities, n_clusters
-clt = KMeans(n_clusters = 5)
-clt.fit(im)
+def clusterColor(im):
+	
+	for i in xrange(2):
+		im = cv2.pyrDown(im)
+		cv2.imshow('im', im)
 
-# build histogram
-hist = centroid_histogram(clt)
-bar = plot_colors(hist, clt.cluster_centers_)
+	# im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+	# reshape im to a vector of pixels
+	im = im.reshape((im.shape[0] * im.shape[1], 3))
+	pixels = []
+	for p in im:
+		if list(p)!=[0,0,0]:
+			pixels.append(p)
+	im = np.array(pixels)	
 
-cv2.imshow('bar', bar)
-cv2.waitKey(0)
+	# cluster the pixel intensities, n_clusters
+	clt = KMeans(n_clusters = 10)
+	clt.fit(im)
+
+	# build histogram
+	hist = centroid_histogram(clt)
+	bar = plot_colors(hist, clt.cluster_centers_)
+
+	cv2.imshow('bar', bar)
+	cv2.waitKey(0)
+
+if __name__=="__main__":
+	im = cv2.imread('data/1.jpg')
+	clusterColor(im)
